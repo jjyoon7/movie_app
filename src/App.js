b271c457288ccfbe16249f274f31dd6dd34e5767
jjyoon7 <jay.yoon7@gmail.com>
2019-10-20 21:32:29 +0200

    add Prop-types to check the property requirement

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import Movie from './Movie';
+import PropTypes from 'prop-types'
 
 const movieData = [
   {
     id: 1,
     name: "blade",
+    rating: 4,
     img: "https://eu.movieposter.com/posters/archive/main/22/A70-11370"
   }, 
   {
     id: 2,
     name: "what ever this is",
+    rating: 2,
     img: "https://eu.movieposter.com/posters/archive/main/50/MPW-25074"
   }, 
   {
     id: 3,
     name: "the shining",
+    rating: 4,
     img: "https://eu.movieposter.com/posters/archive/main/12/A70-6457"
   }, 
   {
     id: 4,
     name: "anchorman",
+    rating: 3.5,
     img: "https://eu.movieposter.com/posters/archive/main/72/MPW-36265"
   }
 ];
+
+Movie.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  img: PropTypes.string.isRequired
+}
 
 function App() {
   return (
     <div className="App">
       {movieData.map(movie => (
-        <Movie key={movie.id} title={movie.name} img={movie.img} />
+        <Movie key={movie.id} title={movie.name} img={movie.img} rating={movie.rating} />
       ))}
     </div>
   );
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 
-export default function Movie({ title, img }){
+export default function Movie({ title, img, rating }){
     return <div>
                 <h2>watch {title} now</h2>
+                <h4>{rating}/5</h4>
                 <img src={img} alt={title}/>
             </div>
 }
