e9c07e2e5771f6c5730e4580d2ccb639bbaab93c
jjyoon7 <jay.yoon7@gmail.com>
2019-10-20 18:37:19 +0200

    dynamic component generation -> use map function on data object array and render it with Movie component to generate dynamic component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import Movie from './Movie';
+
+const movieData = [
+  {
+    name: "blade",
+    img: "https://eu.movieposter.com/posters/archive/main/22/A70-11370"
+  }, 
+  {
+    name: "what ever this is",
+    img: "https://eu.movieposter.com/posters/archive/main/50/MPW-25074"
+  }, 
+  {
+    name: "the shining",
+    img: "https://eu.movieposter.com/posters/archive/main/12/A70-6457"
+  }, 
+  {
+    name: "anchorman",
+    img: "https://eu.movieposter.com/posters/archive/main/72/MPW-36265"
+  }
+];
 
 function App() {
   return (
     <div className="App">
+      {movieData.map(movie => (
+        <Movie title={movie.name} img={movie.img} />
+      ))}
     </div>
   );
 }
