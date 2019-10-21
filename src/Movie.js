1c9118351142c0ed5cf92861f578b4667b3c818c
jjyoon7 <jay.yoon7@gmail.com>
2019-10-21 11:28:43 +0200

    add useState in the App component

--- src/Movie.js
+++ src/Movie.js
@@ -2,9 +2,5 @@ import React from 'react'
 
 
 export default function Movie({ name, img, rating }){
-    return <div>
-                <h2>watch {name} now</h2>
-                <h4>{rating}/5</h4>
-                <img src={img} alt={name}/>
-            </div>
+    return 
 }
