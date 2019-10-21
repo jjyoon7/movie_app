35a9237b23bea22f32922e6d158e58a1d66102a6
jjyoon7 <jay.yoon7@gmail.com>
2019-10-21 11:45:13 +0200

    fetch movie data from YTS API with axios, in the useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Movie from './Movie';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 Movie.propTypes = {
   name: PropTypes.string.isRequired,
@@ -11,8 +12,13 @@ Movie.propTypes = {
 function App() {
   const [movie, setMovie] = useState("")
 
+  useEffect(() => {
+    axios.get("https://yts.lt/api/v2/list_movies.json");
+    console.log("useEffect is called")
+  })
   return (
     <div className="App">
+
     </div>
   );
 }
