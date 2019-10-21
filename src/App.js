caad112b0e8ea58b0ffe9dd55f1d423a4f11ba08
jjyoon7 <jay.yoon7@gmail.com>
2019-10-21 19:01:39 +0200

    add genre data in Movie and App component

--- src/App.js
+++ src/App.js
@@ -22,17 +22,17 @@ export default function App() {
   }, [])
   
   const movieMap = movies.map(movie => (
-    <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}  />
+    <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
   ))
 
   return (
-    <section class="container">
+    <section className="container">
       {isLoading ? (
-        <div class="loader">
-          <span class="looader-text">Loading...</span>
+        <div className="loader">
+          <span className="looader-text">Loading...</span>
         </div>
       ) : (
-        <div class="movies">
+        <div className="movies">
           {movieMap}
         </div>
       )}
--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import './Movie.css'
 
-export default function Movie({ year, title, summary, poster }){
-    return <div class="movie">
-                <img class="movie-poster" src={poster} alt={title} title={title}></img>
-                <div class="movie-data">
-                    <h2 class="movie-title">{title}</h2>
-                    <h4 class="movie-year">{year}</h4>
-                    <p class="movie-summary">{summary}</p>
+export default function Movie({ year, title, summary, poster, genres }){
+    return <div className="movie">
+                <img className="movie-poster" src={poster} alt={title} title={title}></img>
+                <div className="movie-data">
+                    <h2 className="movie-title">{title}</h2>
+                    <h4 className="movie-year">{year}</h4>
+                    <h5 className="movie-genre">{genres}</h5>
+                    <p className="movie-summary">{summary}</p>
                 </div>
             </div>
 }
@@ -18,5 +19,6 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
